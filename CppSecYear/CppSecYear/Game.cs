caa3e266e8293433247ecb28d5b7ec12bf85e1e0
dencheerx <denchir@yandex.ru>
2020-10-30 18:58:27 +0300

    ver 0.01

--- CppSecYear/CppSecYear/Game.cs
+++ CppSecYear/CppSecYear/Game.cs
@@ -11,14 +11,18 @@ namespace CppSecYear
 {
     class Game
     {
+        enum TicTacToe { empty, cross, circle };
+
+        TicTacToe[,] ticTacToes = new TicTacToe[3, 3];
+
         PictureBox field;
 
         Bitmap frame;
         Graphics g;
 
         int fieldDrawSize => Math.Min(width, height);
-        Point fieldCoord => width > height 
-            ? new Point((width - fieldDrawSize) / 2, 0) 
+        Point fieldCoord => width > height
+            ? new Point((width - fieldDrawSize) / 2, 0)
             : new Point(0, (height - fieldDrawSize) / 2);
 
         public Game(PictureBox pb)
@@ -32,8 +36,9 @@ namespace CppSecYear
 
         public void setNewFrame()
         {
-            //g.Clear(Color.White);
+            g.Clear(Color.White);
 
+            #region Drawing lines
             g.DrawLine(fieldPen,
                 fieldDrawSize / 3 + fieldCoord.X,
                 0 + fieldCoord.Y,
@@ -53,8 +58,20 @@ namespace CppSecYear
             g.DrawLine(fieldPen,
                 0 + fieldCoord.X,
                 fieldDrawSize * 2 / 3 + fieldCoord.Y,
-                fieldDrawSize+ fieldCoord.X,
-                fieldDrawSize * 2 / 3 + fieldCoord.Y );
+                fieldDrawSize + fieldCoord.X,
+                fieldDrawSize * 2 / 3 + fieldCoord.Y);
+            #endregion
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Point figureCoord = 
+                        new Point(fieldDrawSize * x / 3, fieldDrawSize  * y / 3);
+                    figureCoord.Offset(fieldCoord);
+                    DrawFigure(figureCoord, ticTacToes[x, y]); 
+                }
+            }
 
             field.Image = frame;
         }
@@ -69,28 +86,53 @@ namespace CppSecYear
                 return;
             }
 
-            clickCoord.X -= ((clickCoord.X - fieldCoord.X) % (fieldDrawSize / 3));
-            clickCoord.Y -= ((clickCoord.Y - fieldCoord.Y) % (fieldDrawSize / 3));
+            int[] cell = ClickToRowNColum(clickCoord);
 
-            DrawFigure(clickCoord);
+            if (ticTacToes[cell[0], cell[1]] != TicTacToe.empty)
+            {
+                return;
+            }
 
-            whichTurn = whichTurn == PlayerTurns.XPlayer ? PlayerTurns.OPlayer : PlayerTurns.XPlayer;
+            if (whichTurn == PlayerTurns.XPlayer)
+            {
+                ticTacToes[cell[0], cell[1]] = TicTacToe.cross;
+            }
+            else
+            {
+                ticTacToes[cell[0], cell[1]] = TicTacToe.circle;
+            }
+
+            whichTurn =
+                whichTurn == PlayerTurns.XPlayer
+                ? PlayerTurns.OPlayer
+                : PlayerTurns.XPlayer;
 
             field.Invalidate();
         }
 
-        void DrawFigure(Point coord)
+        void DrawFigure(Point coord, TicTacToe ttt)
         {
             int size = fieldDrawSize / 3;
-            if (whichTurn == PlayerTurns.XPlayer)
+            if (ttt == TicTacToe.cross)
             {
                 g.DrawLine(fieldPen, coord.X, coord.Y, coord.X + size, coord.Y + size);
                 g.DrawLine(fieldPen, coord.X, coord.Y + size, coord.X + size, coord.Y);
             }
-            else
+            else if (ttt == TicTacToe.circle)
             {
                 g.DrawEllipse(fieldPen, coord.X, coord.Y, size, size);
             }
+
+        }
+
+        int[] ClickToRowNColum(Point coord)
+        {
+            int[] result = new int[2];
+
+            result[0] = (coord.X - fieldCoord.X) / (fieldDrawSize / 3);
+            result[1] = (coord.Y - fieldCoord.Y) / (fieldDrawSize / 3);
+
+            return result;
         }
     }
 }
